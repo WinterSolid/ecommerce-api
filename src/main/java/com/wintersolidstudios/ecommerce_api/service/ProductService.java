b75ea34f0298987bab00a8d51a9b795ef2b0cdc7
ZakTan <zakee.tanks@gmail.com>
2026-06-11 22:10:22 -0500

    implement product service w/ create method

--- src/main/java/com/wintersolidstudios/ecommerce_api/service/ProductService.java
+++ src/main/java/com/wintersolidstudios/ecommerce_api/service/ProductService.java
@@ -1,5 +1,26 @@
 package com.wintersolidstudios.ecommerce_api.service;
 
+import com.wintersolidstudios.ecommerce_api.entity.Product;
+import com.wintersolidstudios.ecommerce_api.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
-    // TODO: implement methods create,get delete products
+
+    private final ProductRepository repository;
+
+    public ProductService(
+            ProductRepository repository
+    ) {
+        this.repository = repository;
+    }
+
+    public Product createProduct(
+            Product product
+    ) {
+        return repository.save(product);
+    }
+
 }
