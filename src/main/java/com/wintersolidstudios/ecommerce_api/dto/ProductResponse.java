5c03d49948798f9c7c0394bda0c9b1ae55fc2787
ZakTan <zakee.tanks@gmail.com>
2026-06-11 21:48:35 -0500

    created product response dto ProductResponse.java

---
+++ src/main/java/com/wintersolidstudios/ecommerce_api/dto/ProductResponse.java
@@ -0,0 +1,23 @@
+package com.wintersolidstudios.ecommerce_api.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductResponse {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Double price;
+
+    private String category;
+
+    private Integer stockQuantity;
+}
