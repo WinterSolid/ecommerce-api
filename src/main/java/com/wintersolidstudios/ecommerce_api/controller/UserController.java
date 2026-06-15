3b9fb15c2ce5a89e0ab07dc0503c90378b317e62
ZakTan <zakee.tanks@gmail.com>
2026-06-15 12:14:51 -0500

    creat Controller and Servie files for user - status: Empty templates

---
+++ src/main/java/com/wintersolidstudios/ecommerce_api/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.wintersolidstudios.ecommerce_api.controller;
+
+public class UserController {
+}
---
+++ src/main/java/com/wintersolidstudios/ecommerce_api/service/UserService.java
@@ -0,0 +1,4 @@
+package com.wintersolidstudios.ecommerce_api.service;
+
+public class UserService {
+}
