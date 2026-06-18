213f41cb2d1866938591d97de3b457de83c30344
ZakTan <zakee.tanks@gmail.com>
2026-06-18 12:20:25 -0500

    User Controller,User  Service files - status: Completed

--- src/main/java/com/wintersolidstudios/ecommerce_api/controller/UserController.java
+++ src/main/java/com/wintersolidstudios/ecommerce_api/controller/UserController.java
@@ -1,4 +1,53 @@
 package com.wintersolidstudios.ecommerce_api.controller;
 
+import com.wintersolidstudios.ecommerce_api.dto.*;
+import com.wintersolidstudios.ecommerce_api.entity.User;
+import com.wintersolidstudios.ecommerce_api.service.UserService;
+
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public UserResponse createUser(@Valid @RequestBody CreateUserRequest request) {
+
+        User user = User.builder()
+                .name(request.getName())
+                .email(request.getEmail())
+                .build();
+
+        User saved = service.createUser(user);
+
+        return UserResponse.builder()
+                .id(saved.getId())
+                .name(saved.getName())
+                .email(saved.getEmail())
+                .build();
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return service.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return service.getUserById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        service.deleteUser(id);
+    }
 }
--- src/main/java/com/wintersolidstudios/ecommerce_api/service/UserService.java
+++ src/main/java/com/wintersolidstudios/ecommerce_api/service/UserService.java
@@ -1,4 +1,34 @@
 package com.wintersolidstudios.ecommerce_api.service;
 
+import com.wintersolidstudios.ecommerce_api.entity.User;
+import com.wintersolidstudios.ecommerce_api.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public User createUser(User user) {
+        return repository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return repository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public void deleteUser(Long id) {
+        repository.deleteById(id);
+    }
 }
